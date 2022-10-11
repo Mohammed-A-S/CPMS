29f1491d79f488f5675fee5943981e635dd988f0
Mohammed A. Alfaifi <92150052+Mohammed-A-S@users.noreply.github.com>
2022-10-11 11:08:09 +0300

    Version 1

---
+++ Script/script.js
@@ -0,0 +1,81 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=>
+	{
+		const li = item.parentElement;
+		
+		item.addEventListener('click', function()
+		{
+			allSideMenu.forEach(i=> 
+				{
+					i.parentElement.classList.remove('active');
+				})
+			li.classList.add('active');
+		})
+	});
+
+
+
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-border-left');
+const sidebar = document.getElementById('sidebar');
+const sidebar_text = document.querySelector('#sidebar .text');
+
+menuBar.addEventListener('click', function () {
+	sidebar.classList.toggle('hide');
+	sidebar_text.classList.style.display = "none";
+})
+
+
+
+
+
+
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+searchButton.addEventListener('click', function (e) {
+	if(window.innerWidth < 576) {
+		e.preventDefault();
+		searchForm.classList.toggle('show');
+		if(searchForm.classList.contains('show')) {
+			searchButtonIcon.classList.replace('nav-link', 'bx-border-left');
+		} else {
+			searchButtonIcon.classList.replace('bx-border-left', 'nav-link');
+		}
+	}
+})
+
+
+
+
+
+if(window.innerWidth < 768) {
+	sidebar.classList.add('hide');
+} else if(window.innerWidth > 576) {
+	searchButtonIcon.classList.replace('bx-border-left', 'nav-link');
+	searchForm.classList.remove('show');
+}
+
+
+window.addEventListener('resize', function () {
+	if(this.innerWidth > 576) {
+		searchButtonIcon.classList.replace('bx-border-left', 'nav-link');
+		searchForm.classList.remove('show');
+	}
+})
+
+
+
+const switchMode = document.getElementById('switch-mode');
+
+switchMode.addEventListener('change', function () {
+	if(this.checked) {
+		document.body.classList.add('dark');
+	} else {
+		document.body.classList.remove('dark');
+	}
+})
---
+++ inc/connction.php
@@ -0,0 +1,12 @@
+<?PHP
+$db_Server_name = "localhost";
+$db_User_name = "root";
+$db_Password = "";
+$db_Name = "jfshj";
+
+$conn = mysqli_connect($db_Server_name, $db_User_name, $db_Password, $db_Name);
+
+if(!$conn)
+{
+    echo 'Error: ' . mysqli_connect_error();
+}
